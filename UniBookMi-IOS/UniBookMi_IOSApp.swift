e2241921247c000950577d8dc863b7d9c6a74511
Giulia Floris <giuliafloris88@gmail.com>
2024-03-22 15:46:22 +0100

    [ADD] initial files

---
+++ UniBookMi-IOS/UniBookMi_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniBookMi_IOSApp.swift
+//  UniBookMi-IOS
+//
+//  Created by giulia.floris on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct UniBookMi_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
